783ad2c85c3e1a5903c736dd5e37f2bd3838eddd
idleberg <git@idleberg.com>
2021-04-05 00:19:59 +0200

    update development files

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import { babel } from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import scss from 'rollup-plugin-scss'
+import typescript from '@rollup/plugin-typescript';
+
+const plugins = [
+  babel({
+    extensions: ['.cjs', '.js', '.jsx', '.mjs', '.ts', '.tsx'],
+    babelHelpers: 'bundled',
+  }),
+  commonjs(),
+  json(),
+  nodeResolve({
+    preferBuiltins: true
+  }),
+  (
+    process.env.ROLLUP_WATCH
+      ? undefined
+      : terser()
+  ),
+  scss({
+    output: false
+  }),
+  typescript({
+    allowSyntheticDefaultImports: true,
+    moduleResolution: "node",
+    resolveJsonModule: true
+  })
+];
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'lib/notify',
+      exports: 'auto',
+      format: 'cjs',
+      sourcemap: true
+    },
+    external: [
+      'atom',
+      'node-notifier'
+    ],
+    plugins: plugins
+  }
+];
