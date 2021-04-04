04f239a183f4e5283e9e0a82dc22454706c9dd09
idleberg <git@idleberg.com>
2021-04-05 01:00:24 +0200

    fix config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,26 +2,19 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
-    es6: true,
-    node: true
+    browser: true
   },
   extends: [
-    'atomic',
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:json/recommended'
   ],
-  globals: {
-    atom: "readonly"
-  },
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
   ],
   rules: {},
   ignorePatterns: [
-    'bower_components',
-    'lib',
-    'node_modules'
+    'lib'
   ]
 };
